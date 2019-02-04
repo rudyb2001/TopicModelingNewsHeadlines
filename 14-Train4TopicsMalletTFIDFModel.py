801e2d566e7faf335d1fbf5dc9fbb4e8a736bb91
Atanu Banerjee <atanu.banerjee@outlook.com>
2019-02-03 19:14:33 -0800

    Updates

---
+++ 14-Train4TopicsMalletTFIDFModel.py
@@ -0,0 +1,20 @@
+import os
+from gensim.models.wrappers import LdaMallet
+os.environ.update({'MALLET_HOME':r'C:/mallet-2.0.8/'})
+# os.environ.update({'PATH':r'C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git\cmd;C:\Users\atanu\AppData\Local\Microsoft\WindowsApps;;C:\Program Files\Microsoft VS Code\bin'})
+mallet_path = 'C:/mallet-2.0.8/bin/mallet'
+
+import pickle
+import gensim
+from gensim import corpora, models
+from multiprocessing import freeze_support
+
+with open('./pickle/corpus_tfidf.pkl', 'rb') as f:
+    corpus_tfidf = pickle.load(f)
+
+with open('./pickle/dictionary.pkl', 'rb') as f:
+    dictionary = pickle.load(f)
+
+lda_model_4topics_mallet_tfidf = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus_tfidf, num_topics=4, id2word=dictionary)
+
+lda_model_4topics_mallet_tfidf.save('./pickle/lda_model_4topics_mallet_tfidf')
---
+++ 16-Wordcloud4TopicsMalletTFIDF.py
@@ -0,0 +1,41 @@
+import pickle
+import gensim
+from gensim import corpora, models
+from multiprocessing import freeze_support
+
+lda_model_4topics_mallet_tfidf = gensim.models.wrappers.LdaMallet.load('./pickle/lda_model_4topics_mallet_tfidf')
+
+# Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=gensim.parsing.preprocessing.STOPWORDS,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model_4topics_mallet_tfidf.show_topics(formatted=False)
+
+fig, axes = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i+1), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.savefig('./DataViz/Wordcloud4TopicsMalletTFIDF.png')
+plt.show()
