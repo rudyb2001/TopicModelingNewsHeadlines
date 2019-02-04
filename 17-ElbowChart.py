3fd9103ce58fa305a6b434a1f2a803146a74974f
Atanu Banerjee <atanu.banerjee@outlook.com>
2019-02-03 20:47:32 -0800

    Updates

--- 17-ElbowChart.py
+++ 17-ElbowChart.py
@@ -5,59 +5,62 @@ import gensim
 from gensim.models.coherencemodel import CoherenceModel
 from matplotlib import pyplot as plt
 
-with open('./pickle/corpus_tfidf.pkl', 'rb') as f:
-    corpus_tfidf = pickle.load(f)
-
-with open('./pickle/dictionary.pkl', 'rb') as f:
-    dictionary = pickle.load(f)
-
-import pandas as pd
-processed_docs = pd.read_pickle("./pickle/processed_docs.pkl")
-
 import os
 os.environ.update({'MALLET_HOME':r'C:/mallet-2.0.8/'})
 mallet_path = 'C:/mallet-2.0.8/bin/mallet'
 
-def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
-    """
-    Compute c_v coherence for various number of topics
+if __name__ == '__main__': 
 
-    Parameters:
-    ----------
-    dictionary : Gensim dictionary
-    corpus : Gensim corpus
-    texts : List of input texts
-    limit : Max num of topics
+    with open('./pickle/corpus_tfidf.pkl', 'rb') as f:
+        corpus_tfidf = pickle.load(f)
 
-    Returns:
-    -------
-    model_list : List of LDA topic models
-    coherence_values : Coherence values corresponding to the LDA model with respective number of topics
-    """
-    coherence_values = []
-    model_list = []
+    with open('./pickle/dictionary.pkl', 'rb') as f:
+        dictionary = pickle.load(f)
 
-    mallet_path = 'C:/mallet-2.0.8/bin/mallet'
+    import pandas as pd
+    processed_docs = pd.read_pickle("./pickle/processed_docs.pkl")
 
-    for num_topics in range(start, limit, step):
-        model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=dictionary)
-        model_list.append(model)
-        coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
-        coherence_score = coherencemodel.get_coherence()
-        coherence_values.append(coherence_score)
-        print("Num Topics = ", num_topics, " has Coherence Value of ", coherence_score)
 
-    return model_list, coherence_values
+    def compute_coherence_values(dictionary, corpus, texts, limit, start=2, step=3):
+        """
+        Compute c_v coherence for various number of topics
 
-# Can take a long time to run.
-model_list, coherence_values = compute_coherence_values(dictionary=dictionary, corpus=corpus_tfidf, texts=processed_docs, start=2, limit=20, step=2)
+        Parameters:
+        ----------
+        dictionary : Gensim dictionary
+        corpus : Gensim corpus
+        texts : List of input texts
+        limit : Max num of topics
 
-# Show graph
-limit=20; start=2; step=2
-x = range(start, limit, step)
-plt.plot(x, coherence_values)
-plt.xlabel("Num Topics")
-plt.ylabel("Coherence score")
-plt.legend(("coherence_values"), loc='best')
-plt.savefig('./DataViz/ElbowChartCoherenceScores.png')
-plt.show()
+        Returns:
+        -------
+        model_list : List of LDA topic models
+        coherence_values : Coherence values corresponding to the LDA model with respective number of topics
+        """
+        coherence_values = []
+        model_list = []
+
+        mallet_path = 'C:/mallet-2.0.8/bin/mallet'
+
+        for num_topics in range(start, limit, step):
+            model = gensim.models.wrappers.LdaMallet(mallet_path, corpus=corpus, num_topics=num_topics, id2word=dictionary)
+            model_list.append(model)
+            coherencemodel = CoherenceModel(model=model, texts=texts, dictionary=dictionary, coherence='c_v')
+            coherence_score = coherencemodel.get_coherence()
+            coherence_values.append(coherence_score)
+            print("Num Topics = ", num_topics, " has Coherence Value of ", coherence_score)
+
+        return model_list, coherence_values
+
+    # Can take a long time to run.
+    model_list, coherence_values = compute_coherence_values(dictionary=dictionary, corpus=corpus_tfidf, texts=processed_docs, start=2, limit=20, step=2)
+
+    # Show graph
+    limit=20; start=2; step=2
+    x = range(start, limit, step)
+    plt.plot(x, coherence_values)
+    plt.xlabel("Num Topics")
+    plt.ylabel("Coherence score")
+    plt.legend(("coherence_values"), loc='best')
+    plt.savefig('./DataViz/ElbowChartCoherenceScores.png')
+    plt.show()
